e88139d523edeb9d3aa5d42e39c379b49d8354f0
Theniconico <dariusope@gmail.com>
2020-12-08 19:51:01 -0300

    arreglado

--- layouts/header.php
+++ layouts/header.php
@@ -12,7 +12,7 @@
 
             <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
               <div class="site-logo">
-              <img src="" alt="">
+              <img src="./images/logo.png" alt="">
               </div>
             </div>
 
