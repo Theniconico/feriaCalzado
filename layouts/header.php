d0d7a61dc3d627defde2fd8f9e1778ab9ac44677
fernandotroncoso94 <fernandotroncoso94@gmail.com>
2020-12-09 06:55:32 -0300

    Modificación de LogIn

--- layouts/header.php
+++ layouts/header.php
@@ -57,12 +57,3 @@
         </div>
       </nav>
     </header>
-
-    <div class="bg-light py-3">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 mb-0"><a href="index.php">Inicio</a> <span class="mx-2 mb-0">/</span>
-           <strong class="text-black">Tienda</strong></div>
-        </div>
-      </div>
-    </div>
