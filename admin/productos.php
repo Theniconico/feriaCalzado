5c2c03afee088634ed491a9a2ac72bb3a7356f5d
Theniconico <dariusope@gmail.com>
2020-12-09 08:05:44 -0300

    borre el12 qlo feo

--- admin/productos.php
+++ admin/productos.php
@@ -338,18 +338,6 @@ $resultado = $conexion->query("
                 <input type="text" name="descripcion" placeholder="DESCRIPCIÓN" id="descripcionEdit" class="form-control" required>
               </div>
 
-              <button class="accordion">numero de calzado</button>
-              <div class="panel">
-                <div class="input-group-prepend">
-                  <div class="input-group">
-                    <input type="checkbox"/>
-                    <label >12</label>
-                    <input type="number" placeholder="Cantidad" class="form-control">
-                  </div>
-                </div>
-
-              </div>
-
               <div class="form-group">
                 <label for="precio_compraEdit">Precio de compra</label>
                 <input type="number" min="0" name="precio_compra" placeholder="Precio de compra" id="precio_compraEdit" class="form-control" required>
