b26a17349aca0ffa92d4a2db7b641aba1058639d
Theniconico <75449759+Theniconico@users.noreply.github.com>
2021-01-11 18:46:53 -0300

    addstock(talla)

--- php/addstock.php
+++ php/addstock.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
 include "./conexion.php";
-        $num_calzado = $_POST['calzado'];
-        $stockCalzado = $_POST['stock'];
-        $conexion->query("delete from num_calzado where id_num =".$_POST['id']);
+$num_calzado = $_POST['calzado'];
+$stockCalzado = $_POST['stock'];
+$conexion->query("delete from num_calzado where id_num =" . $_POST['id']);
+$values = '';
+for ($i = 0; $i < sizeof($num_calzado); $i++) {
+    $id = $i + 1;
 
-        for ($i=0; $i < sizeof($num_calzado); $i++) {
-            $id = $i+1;
-            $aux = "insert into num_calzado values(
-                ".$id.",
-                 ".$_POST['id'].",
-                 '".$num_calzado[$i]."'
-            ),";
-             $conexion->query($aux);
-            // echo "insert into num_calzado values(
-            //     ".$id.",
-            //     ".$_POST['id'].",
-            //     '".$num_calzado[$i]."'
-            // )<br>";
-        }
-        
-    //   header("Location: ../admin/productos.php");
+    $values .= "(" . $id . ",
+            " . $id  . ",
+            '" . $num_calzado[$i] . "',
+            '" . $_POST['id'] . "'
+       ),";
+}
+/* echo $values; */
+$consulta = "INSERT INTO num_calzado (id, id_num, numeros, producto_fk) VALUES ";
+$values = substr($values, 0, -1);
+/* echo $values.";"; */
+$consulta_final = $consulta . $values . ";";
+echo $consulta_final;
+$conexion->query($consulta_final);
 ?>
