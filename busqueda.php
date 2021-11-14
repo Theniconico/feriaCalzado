ca2e221b51e6f1455aabc3df2fbfbbfa8b36e6d7
Theniconico <75449759+Theniconico@users.noreply.github.com>
2021-11-13 23:28:28 -0300

    13-11-21 en la mañana

--- busqueda.php
+++ busqueda.php
@@ -1,141 +1,230 @@
-<?php 
-    include('./php/conexion.php');
-    if (!isset($_GET['texto'])) {
-        header("Location: ./index.php");
-    }
+<?php
+include('./php/conexion.php');
+if (!isset($_GET['texto'])) {
+  header("Location: ./index.php");
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Busqueda</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<html lang="es">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+<head>
+  <title>La Feria del calzado</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
 
 
-    <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="css/aos.css">
 
-    <link rel="stylesheet" href="css/style.css">
-    
-  </head>
-  <body>
-  
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
   <div class="site-wrap">
-    <?php include("./layouts/header.php"); ?> 
+    <?php include("./layouts/header.php"); ?>
 
     <div class="site-section">
       <div class="container">
 
         <div class="row mb-5">
           <div class="col-md-9 order-2">
-           <!-- Producto -->
-           <div class="row mb-5">
-              <?php
-                include('./php/conexion.php');
-                $limite = 10;//productos por pagina
-                $totalQuery = $conexion->query("SELECT count(*) FROM productos")or die($conexion->error);
-                $totalProductos = mysqli_fetch_row($totalQuery);
-                $totalBotones = round($totalProductos[0] / $limite);
-                if (isset($_GET['limite'])) {
-                  $resultado = $conexion -> query("SELECT * FROM productos WHERE estado = 1 LIMIT ".$_GET['limite'].",".$limite) or die($conexion -> error);
-                }else {
-                  $resultado = $conexion -> query("SELECT * FROM productos WHERE estado = 1 LIMIT ".$limite) or die($conexion -> error);
-                }
-                while ($fila = mysqli_fetch_array($resultado)) {
-                  
-                
-              ?>
-              <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                <div class="block-4 text-center border">
-                  <figure class="block-4-image">
-                    <a href="shop-single.php?id=<?php echo $fila['id'];?>">
-                    <img src="images/referencia/img_feria/<?php echo $fila['imagen'];?>" alt="<?php echo $fila['nombre'] ;?>" class="img-fluid"></a>
-                  </figure>
-                  <div class="block-4-text p-4">
-                    <h3><a href="shop-single.php?id=<?php echo $fila['id'];?>"><?php echo $fila['nombre'] ;?></a></h3>
-                    <p class="mb-0"><?php echo $fila['descripcion'] ;?></p>
-                    <p class="text-primary font-weight-bold">$<?php echo $fila['precio_venta'] ;?></p>
+
+            <div class="row">
+              <div class="col-md-12 mb-5">
+                <div class="float-md-left mb-4">
+                  <h2 class="text-black h5">Buscando resultados para <?php echo $_GET['texto']; ?></h2>
+                </div>
+                <div class="d-flex">
+                  <div class="dropdown mr-1 ml-md-auto">
+                    <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Latest
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
+                      <a class="dropdown-item" href="#">Men</a>
+                      <a class="dropdown-item" href="#">Women</a>
+                      <a class="dropdown-item" href="#">Children</a>
+                    </div>
+                  </div>
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" id="dropdownMenuReference" data-toggle="dropdown">Reference</button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuReference">
+                      <a class="dropdown-item" href="#">Relevance</a>
+                      <a class="dropdown-item" href="#">Name, A to Z</a>
+                      <a class="dropdown-item" href="#">Name, Z to A</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Price, low to high</a>
+                      <a class="dropdown-item" href="#">Price, high to low</a>
+                    </div>
                   </div>
                 </div>
               </div>
-                <?php }?>
-           
             </div>
-            <!-- Fin Producto -->
-            <div class="col-md-12 text-center">
+            <div class="row mb-5">
+              <!-- Producto -->
+              <?php
+
+              $resultado = $conexion->query("SELECT * FROM productos WHERE 
+                nombre like '%" . $_GET['texto'] . "%' or
+                descripcion like '%" . $_GET['texto'] . "%' or
+                color like '%" . $_GET['texto'] . "%'
+                ORDER BY id DESC LIMIT 10") or die($conexion->error);
+              if (mysqli_num_rows($resultado) > 0) {
+
+
+                while ($fila = mysqli_fetch_array($resultado)) {
+
+
+              ?>
+                  <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
+                    <div class="block-4 text-center border">
+                      <figure class="block-4-image">
+                        <a href="shop-single.php?id=<?php echo $fila['id']; ?>">
+                          <img src="images/referencia/img_feria/<?php echo $fila['imagen']; ?>" alt="<?php echo $fila['nombre']; ?>" class="img-fluid"></a>
+                      </figure>
+                      <div class="block-4-text p-4">
+                        <h3><a href="shop-single.php?id=<?php echo $fila['id']; ?>"><?php echo $fila['nombre']; ?></a></h3>
+                        <p class="mb-0"><?php echo $fila['descripcion']; ?></p>
+                        <p class="text-primary font-weight-bold">$<?php echo $fila['precio_venta']; ?></p>
+                      </div>
+                    </div>
+                  </div>
+              <?php }
+              } else {
+                echo '<h2>Sin resultados</h2>';
+              } ?>
+              <!-- Fin Producto -->
+            </div>
+            <div class="row" data-aos="fade-up">
+              <div class="col-md-12 text-center">
                 <div class="site-block-27">
                   <ul>
-                    <?php
-                    if (isset($_GET['limite'])) {
-                      if ($_GET['limite'] > 0) {
-                        echo '<li><a href="index.php?limite='.($_GET['limite'] - 10).'">&lt;</a></li>';
-                      }
-                    }
-                      for ($k=0; $k < $totalBotones; $k++) { 
-                        echo '<li><a href="index.php?limite='.($k*10).'">'.($k+1).'</a></li>';
-                      }
-                      if (isset($_GET['limite'])) {
-                          if ($_GET['limite']+10 < $totalBotones*10) {
-                            echo '<li><a href="index.php?limite='.($_GET['limite']+10).'">&gt;</a></li>';
-                          }
-                      }else {
-                        echo '<li><a href="index.php?limite=10">&gt;</a></li>';
-                      }
-                    ?>
-                    
+                    <li><a href="#">&lt;</a></li>
+                    <li class="active"><span>1</span></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">&gt;</a></li>
                   </ul>
                 </div>
               </div>
             </div>
-            <div class="col-md-3 order-1 mb-5 mb-md-0">
-          <div class="border p-4 rounded mb-4">
-              <h3 class="mb-3 h6 text-uppercase text-black d-block">Categorias</h3>
+          </div>
+
+          <div class="col-md-3 order-1 mb-5 mb-md-0">
+            <div class="border p-4 rounded mb-4">
+              <h3 class="mb-3 h6 text-uppercase text-black d-block">Categories</h3>
               <ul class="list-unstyled mb-0">
-               <?php 
-               $re = $conexion ->query("select * from categorias");
-               while($f =mysqli_fetch_array($re)){
-               ?>
-               <li class="mb-1">
-                 <a href="./busqueda.php?texto=<?php echo $f['nombre']?>" class="d-flex">
-                 <span><?php echo$f['nombre']?></span> 
-                 <span class="text-black ml-auto"><?php
-                 $re2 = $conexion -> query("select count(*) from productos where estado = 1 and categoriaFK = ".$f['id']);
-                 $fila = mysqli_fetch_row($re2);
-                 echo $fila[0];
-                 ?></span>
-                 </a>
-               </li>
-            
-
-
-               <?php }?> 
-            </ul>
+                <li class="mb-1"><a href="#" class="d-flex"><span>Hombre</span> <span class="text-black ml-auto">(2,220)</span></a></li>
+                <li class="mb-1"><a href="#" class="d-flex"><span>Mujer</span> <span class="text-black ml-auto">(2,550)</span></a></li>
+                <li class="mb-1"><a href="#" class="d-flex"><span>Niños</span> <span class="text-black ml-auto">(2,124)</span></a></li>
+              </ul>
             </div>
 
-           
-          </div>
-          </div>
+            <div class="border p-4 rounded mb-4">
+              <div class="mb-4">
+                <h3 class="mb-3 h6 text-uppercase text-black d-block">Filter by Price</h3>
+                <div id="slider-range" class="border-primary"></div>
+                <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white" disabled="" />
+              </div>
 
-          
-          
+              <div class="mb-4">
+                <h3 class="mb-3 h6 text-uppercase text-black d-block">Size</h3>
+                <label for="s_sm" class="d-flex">
+                  <input type="checkbox" id="s_sm" class="mr-2 mt-1"> <span class="text-black">Small (2,319)</span>
+                </label>
+                <label for="s_md" class="d-flex">
+                  <input type="checkbox" id="s_md" class="mr-2 mt-1"> <span class="text-black">Medium (1,282)</span>
+                </label>
+                <label for="s_lg" class="d-flex">
+                  <input type="checkbox" id="s_lg" class="mr-2 mt-1"> <span class="text-black">Large (1,392)</span>
+                </label>
+              </div>
+
+              <div class="mb-4">
+                <h3 class="mb-3 h6 text-uppercase text-black d-block">Color</h3>
+                <a href="#" class="d-flex color-item align-items-center">
+                  <span class="bg-danger color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Red (2,429)</span>
+                </a>
+                <a href="#" class="d-flex color-item align-items-center">
+                  <span class="bg-success color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Green (2,298)</span>
+                </a>
+                <a href="#" class="d-flex color-item align-items-center">
+                  <span class="bg-info color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Blue (1,075)</span>
+                </a>
+                <a href="#" class="d-flex color-item align-items-center">
+                  <span class="bg-primary color d-inline-block rounded-circle mr-2"></span> <span class="text-black">Purple (1,075)</span>
+                </a>
+              </div>
+
+            </div>
+          </div>
         </div>
 
- 
-        
+        <div class="row">
+          <div class="col-md-12">
+            <div class="site-section site-blocks-2">
+              <div class="row justify-content-center text-center mb-5">
+                <div class="col-md-7 site-section-heading pt-4">
+                  <h2>Categories</h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
+                  <a class="block-2-item" href="#">
+                    <figure class="image">
+                      <img src="images/women.jpg" alt="" class="img-fluid">
+                    </figure>
+                    <div class="text">
+                      <span class="text-uppercase">Collections</span>
+                      <h3>Women</h3>
+                    </div>
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+                  <a class="block-2-item" href="#">
+                    <figure class="image">
+                      <img src="images/children.jpg" alt="" class="img-fluid">
+                    </figure>
+                    <div class="text">
+                      <span class="text-uppercase">Collections</span>
+                      <h3>Children</h3>
+                    </div>
+                  </a>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
+                  <a class="block-2-item" href="#">
+                    <figure class="image">
+                      <img src="images/men.jpg" alt="" class="img-fluid">
+                    </figure>
+                    <div class="text">
+                      <span class="text-uppercase">Collections</span>
+                      <h3>Men</h3>
+                    </div>
+                  </a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
-    <?php include("./layouts/footer.php"); ?> 
+    <?php include("./layouts/footer.php"); ?>
 
-    
+
   </div>
 
   <script src="js/jquery-3.3.1.min.js"></script>
@@ -147,6 +236,7 @@
   <script src="js/aos.js"></script>
 
   <script src="js/main.js"></script>
-    
-  </body>
+
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
   <head>
     <title>Tienda</title>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
